9609d97d40d03a6db36590da2f8f59bc1073d78c
Alexandr <romanenyasasha@gmail.com>
2024-06-04 13:28:36 +0300

    Fixed bug with channel size

--- logging/global.go
+++ logging/global.go
@@ -3,6 +3,6 @@ package logging
 var loggerConfigs = LoggerConfigs{}
 
 func MakeGlobalChannel(channelSize uint64) chan *Message {
-	loggerConfigs.ListenChan = make(chan *Message)
+	loggerConfigs.ListenChan = make(chan *Message, channelSize)
 	return loggerConfigs.ListenChan
 }
